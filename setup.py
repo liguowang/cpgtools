d8e8e46ddc6d3970260bf389b999a7fecee9d42e
Liguo Wang <wangliguo78@gmail.com>
2018-11-06 16:47:47 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -7,40 +7,40 @@ Setup script for CpGtools  -- Tools to analyze and visualize DNA methylation dat
 """
 
 def main():
-	setup(  name = "CpGtools",
-			version = "0.1.0",
-                        python_requires='>=3.5',
-			py_modules = [ 'psyco_full' ],
-			packages = find_packages( 'lib' ),
-			package_dir = { '': 'lib' },
-			package_data = { '': ['*.ps'] },
-			scripts = glob.glob( "bin/*.py"),
-			ext_modules = [],
-			test_suite = 'nose.collector',
-			setup_requires = ['nose>=0.10.4','cython>=0.12'],
-			author = "Liguo Wang",
-			author_email ="wangliguo78@gmail.com",
-			platforms = ['Linux','MacOS'],
-			requires = ['cython (>=0.17)'],
-			install_requires = ['cython>=0.17','numpy','pysam','bx-python','pyBigWig','sklearn'], 
-			description = "Tools to analyze and visualize DNA methylation data",
-			url = "http://cpgtools.sourceforge.net/",
-			zip_safe = False,
-			dependency_links = [],
-			classifiers=[
-				'Development Status :: 5 - Production/Stable',
-				'Environment :: Console',
-				'Intended Audience :: Science/Research',
-				'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-				'Operating System :: MacOS :: MacOS X',
-				'Operating System :: POSIX',
-				'Programming Language :: Python',
-				'Topic :: Scientific/Engineering :: Bio-Informatics',
-			],
-			
-			keywords='DNA methylation, CpG, bisulfite sequencing, RRBS, 850K, 450K',
+    setup(  name = "CpGtools",
+            version = "0.1.0",
+            python_requires='>=3.5',
+            py_modules = [ 'psyco_full' ],
+            packages = find_packages( 'lib' ),
+            package_dir = { '': 'lib' },
+            package_data = { '': ['*.ps'] },
+            scripts = glob.glob( "bin/*.py"),
+            ext_modules = [],
+            test_suite = 'nose.collector',
+            setup_requires = ['nose>=0.10.4','cython>=0.12'],
+            author = "Liguo Wang",
+            author_email ="wangliguo78@gmail.com",
+            platforms = ['Linux','MacOS'],
+            requires = ['cython (>=0.17)'],
+            install_requires = ['cython>=0.17','numpy','pysam','bx-python','pyBigWig','sklearn'], 
+            description = "Tools to analyze and visualize DNA methylation data",
+            url = "http://cpgtools.sourceforge.net/",
+            zip_safe = False,
+            dependency_links = [],
+            classifiers=[
+                'Development Status :: 5 - Production/Stable',
+                'Environment :: Console',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                'Operating System :: MacOS :: MacOS X',
+                'Operating System :: POSIX',
+                'Programming Language :: Python',
+                'Topic :: Scientific/Engineering :: Bio-Informatics',
+            ],
+            
+            keywords='DNA methylation, CpG, bisulfite sequencing, RRBS, 850K, 450K',
              )
 
 
 if __name__ == "__main__":
-	main()
+    main()
