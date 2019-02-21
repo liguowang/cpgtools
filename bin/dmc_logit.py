1077372deea32ee5e9be03a5e0a5d8faa08326bd
Liguo Wang <wangliguo78@gmail.com>
2019-02-21 11:13:40 -0600

    update dmc_logit.py

--- bin/dmc_logit.py
+++ bin/dmc_logit.py
@@ -43,8 +43,9 @@ __status__ = "Development"
 def main():
 	usage="%prog [options]" + "\n"
 	parser = OptionParser(usage,version="%prog " + __version__)
-	parser.add_option("-i","--input-file",action="store",type="string",dest="input_file",help="Data file containing methylation proportions (represented by \"methyl_count,total_count\", eg. \"20,30\") with the 1st row containing sample IDs (must be unique) and the 1st column containing CpG positions or probe IDs (must be unique). This file can be regular or compressed by 'gzip' or 'bz'.")
+	parser.add_option("-i","--input-file",action="store",type="string",dest="input_file",help="Data file containing methylation proportions (represented by \"methyl_count,total_count\", eg. \"20,30\") with the 1st row containing sample IDs (must be unique) and the 1st column containing CpG positions or probe IDs (must be unique). This file can be a regular text file or compressed file (*.gz, *.bz2) or accessible url..")
 	parser.add_option("-g","--group",action="store",type="string",dest="group_file",help="Group file define the biological groups of each samples as well as other covariables such as gender, age.  Sample IDs shoud match to the \"Data file\".")
+	parser.add_option("-f","--family",action="store",type="int",dest="family_func",default=1, help="Error distribution and link function to be used in the GLM model. Can be integer 1 or 2 with 1 = \"binomial\" and 2 = \"quasibinomial\". Default=%default.")
 	parser.add_option("-o","--output",action="store",type='string', dest="out_file",help="Prefix of output file.")
 	(options,args)=parser.parse_args()
 	
@@ -63,10 +64,20 @@ def main():
 		print (__doc__)
 		parser.print_help()
 		sys.exit(103)	
+	if not os.path.isfile(options.input_file):
+		print ("Input data file \"%s\" does not exist\n" % options.input_file) 
+		sys.exit(104)
+	if not os.path.isfile(options.group_file):
+		print ("Input group file \"%s\" does not exist\n" % options.input_file) 
+		sys.exit(105)
 	
 	FOUT = open(options.out_file + '.pval.txt','w')
 	ROUT = open(options.out_file + '.r','w')
-	
+	family = {1:'binomial',2:'quasibinomial'}
+	if not options.family_func in family.keys():
+		print ("Incorrect value of '-f'!") 
+		sys.exit(106)
+		
 	printlog("Read group file \"%s\" ..." % (options.group_file))
 	(samples,cv_names, cvs) = read_grp_file2(options.group_file)
 	for cv_name in cv_names:
@@ -109,22 +120,18 @@ def main():
 					else:
 						printlog("Incorrect data format!")
 						print (f)
-						sys.exit(1)		
-				#except:
-				#	betas.append(np.nan)
-				#	total_reads.append(np.nan)
-				
-				
+						sys.exit(1)						
 			print ('',file=ROUT)
-			print ('cgid = \"%s\"' % cg_id, file=ROUT)
+			print ('cgid <- \"!%s\"' % cg_id, file=ROUT)
 			print ("y <- c(%s)" % (','.join([str(read) for read in methyl_reads])), file=ROUT)	#response variable
 			print ("total_reads <- c(%s)" % (','.join([str(read) for read in total_reads])), file=ROUT)	#For a binomial GLM prior weights are used to give the number of trials when the response is the proportion of successes
 			for cv_name in cv_names:
-				print (cv_name + '<- c(%s)' % (','.join([str(cvs[cv_name][s]) for s in  sample_IDs  ])), file = ROUT)
-			print ('fit <- glm(y/total_reads ~ %s, weights=total_reads, family=binomial)' % ('+'.join(cv_names)), file = ROUT)
-			print ('pval = coef(summary(fit))[,4]',file=ROUT)
-			print ('coef = coef(summary(fit))[,1]',file=ROUT)
-			print ('cat(cgid, names(pval),pval,coef, sep="\t")', file=ROUT)
+				print (cv_name + ' <- c(%s)' % (','.join([str(cvs[cv_name][s]) for s in  sample_IDs  ])), file = ROUT)
+			#print ('try(fit <- glm(y/total_reads ~ %s, weights=total_reads, family=binomial))' % ('+'.join(cv_names)), file = ROUT)
+			print ('try(fit <- glm(cbind(y,total_reads-y) ~ %s, family=%s))' % ('+'.join(cv_names), family[options.family_func]), file = ROUT)
+			print ('pval <- coef(summary(fit))[,4]',file=ROUT)
+			print ('coef <- coef(summary(fit))[,1]',file=ROUT)
+			print ('cat(cgid, names(pval),pval,coef, sep="\\t")', file=ROUT)
 			print ('cat("\\n")', file=ROUT)
 	ROUT.close()
 	
@@ -141,17 +148,22 @@ def main():
 	glm_results = {}
 	for l in open(options.out_file + '.r.results.txt'):
 		l = l.strip()
+		if not l.startswith('!'):continue
 		l = l.replace(')','')
 		l = l.replace('(','')
 		f = l.split('\t')
-		cgID = f[0]
+		cgID = f[0].replace('!','')
 		tmp = f[1:]
-		chunk_size = int(len(tmp)/3)
-		sub_lists = [tmp[i:i+chunk_size] for i in range(0,len(tmp),chunk_size)]
-		v_names = sub_lists[0][1:]
-		v_pvals = sub_lists[1][1:]
-		v_coefs = sub_lists[2][1:]
-		glm_results[cgID] = [v_pvals, v_coefs]
+		
+		if len(tmp)%3 == 0:
+			chunk_size = int(len(tmp)/3)
+			sub_lists = [tmp[i:i+chunk_size] for i in range(0,len(tmp),chunk_size)]
+			v_names = sub_lists[0][1:]
+			v_pvals = sub_lists[1][1:]
+			v_coefs = sub_lists[2][1:]
+			glm_results[cgID] = [v_coefs, v_pvals]
+		else:
+			glm_results[cgID] = [["NaN"]* len(cv_names), ["NaN"]* len(cv_names)]
 	
 	printlog("Results saved to \"%s\" ..." % (options.out_file + '.pval.txt'))
 	line_num = 0
@@ -159,7 +171,7 @@ def main():
 		line_num += 1
 		f = l.split()
 		if line_num == 1:
-			print (l + '\t' + '\t'.join([i + '.pval' for i in v_names]) + '\t' + '\t'.join([i + '.coef' for i in v_names]), file=FOUT)
+			print (l + '\t' + '\t'.join([i + '.coef' for i in v_names]) + '\t' + '\t'.join([i + '.pval' for i in v_names]), file=FOUT)
 		else:
 			cgID = f[0]
 			print (l + '\t' + '\t'.join(glm_results[cgID][0]) + '\t' + '\t'.join(glm_results[cgID][1]), file=FOUT)
