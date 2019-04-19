66b63f1c84f5394918b8d74f2133e9f3cbe74c82
Liguo Wang <wangliguo78@gmail.com>
2019-04-19 09:40:49 -0500

    bug fixed

--- bin/dmc_bb.py
+++ bin/dmc_bb.py
@@ -40,7 +40,7 @@ __author__ = "Liguo Wang"
 __copyright__ = "Copyleft"
 __credits__ = []
 __license__ = "GPL"
-__version__="0.1.0"
+__version__="0.1.2"
 __maintainer__ = "Liguo Wang"
 __email__ = "wang.liguo@mayo.edu"
 __status__ = "Development"
@@ -49,10 +49,10 @@ __status__ = "Development"
 def main():
 	usage="%prog [options]" + "\n"
 	parser = OptionParser(usage,version="%prog " + __version__)
-	parser.add_option("-i","--input-file",action="store",type="string",dest="input_file",help="Data file containing methylation proportions (represented by \"methyl_count,total_count\", eg. \"20,30\") with the 1st row containing sample IDs (must be unique) and the 1st column containing CpG positions or probe IDs (must be unique). This file can be a regular text file or compressed file (*.gz, *.bz2) or accessible url..")
-	parser.add_option("-g","--group",action="store",type="string",dest="group_file",help="Group file define the biological groups of each samples as well as other covariables such as gender, age.  Sample IDs shoud match to the \"Data file\".")
-	parser.add_option("-f","--family",action="store",type="int",dest="family_func",default=2, help="A gamlss (https://cran.r-project.org/web/packages/gamlss/index.html) family object. Can be integer 1 or 2 with 1 = \"BB (beta binomial)\" and 2 = \"ZIBB (zero inflated beta binomial)\". Default=%default.")
-	parser.add_option("-o","--output",action="store",type='string', dest="out_file",help="Prefix of output file.")
+	parser.add_option("-i","--input-file",action="store",type="string",dest="input_file",help="Data file containing methylation proportions (represented by \"methyl_count,total_count\", eg. \"20,30\") with the 1st row containing sample IDs (must be unique) and the 1st column containing CpG positions or probe IDs (must be unique). This file can be a regular text file or compressed file (*.gz, *.bz2) or accessible url.")
+	parser.add_option("-g","--group",action="store",type="string",dest="group_file",help="Group file defining the biological groups of each sample as well as other covariables such as gender, age.  Sample IDs should match to the \"Data file\".")
+	parser.add_option("-f","--family",action="store",type="int",dest="family_func",default=1, help="A gamlss (https://cran.r-project.org/web/packages/gamlss/index.html) family object. Can be integer 1 or 2 with 1 = \"BB (beta binomial)\", 2 = \"ZIBB (zero inflated beta binomial)\" or 3 = \"ZABB (zero adjusted beta binomial)\". Default=%default.")
+	parser.add_option("-o","--output",action="store",type='string', dest="out_file",help="Prefix of the output file.")
 	(options,args)=parser.parse_args()
 	
 	print ()
@@ -80,7 +80,7 @@ def main():
 	
 	FOUT = open(options.out_file + '.pval.txt','w')
 	ROUT = open(options.out_file + '.r','w')
-	family = {1:'BB',2:'ZIBB'}
+	family = {1:'BB',2:'ZIBB', 3:'ZABB'}
 	if not options.family_func in family.keys():
 		print ("Incorrect value of '-f'!") 
 		sys.exit(106)
@@ -142,50 +142,57 @@ def main():
 			print ('capture.output(s <- summary(fit, save=TRUE), file="NUL")', file=ROUT)
 			print ('pval <- s$coef.table[,4]',file=ROUT)
 			print ('coef <- s$coef.table[,1]',file=ROUT)
-			print ('cat(cgid, names(pval),as.vector(pval),as.vector(coef), sep="\\t")', file=ROUT)
-			print ('cat("\\n")', file=ROUT)
+			#print ('cat(cgid, names(pval),as.vector(pval),as.vector(coef), sep="\\t")', file=ROUT)
+			print ( 'write.table(file=\"%s\",x=matrix(c(cgid, names(pval),as.vector(pval),as.vector(coef)), nrow=1),quote=FALSE, row.names=FALSE, sep="\\t", col.names=FALSE, append = TRUE)' % (options.out_file + '.tmp_results.txt'),  file = ROUT) 
 	ROUT.close()
 	
-	
+	#sys.exit(0)
 	try:
 		printlog("Runing Rscript file \"%s\" ..." % (options.out_file + '.r'))
-		subprocess.call("Rscript %s >%s 2>%s" % (options.out_file + '.r', options.out_file + '.r.results.txt',options.out_file + '.r.warnings.txt' ), shell=True)
+		subprocess.call("Rscript %s 2>%s" % (options.out_file + '.r', options.out_file + '.tmp_warnings.txt' ), shell=True)
 	except:
 		print ("Error: cannot run Rscript: \"%s\"" % (options.out_file + '.r'), file=sys.stderr)
 		sys.exit(1)
 	
 	
-	printlog("Reading file \"%s\" ..." % (options.out_file + '.r.results.txt'))
+	printlog("Reading file \"%s\" ..." % (options.out_file + '.tmp_results.txt'))
 	bbr_results = {}
-	for l in open(options.out_file + '.r.results.txt'):
+	for l in open(options.out_file + '.tmp_results.txt'):
+		bbr_names = []
+		bbr_pvalues = []
+		bbr_coeffs = []
 		l = l.strip()
 		if not l.startswith('!'):continue
 		l = l.replace(')','')
 		l = l.replace('(','')
-		f = l.split('\t')
+		f = l.split()
 		cgID = f[0].replace('!','')
 		tmp = f[1:]
+		bbr_results[cgID] = [cv_names, ["NaN"]* len(cv_names), ["NaN"]* len(cv_names)]
 		
 		if len(tmp)%3 == 0:
 			chunk_size = int(len(tmp)/3)
 			sub_lists = [tmp[i:i+chunk_size] for i in range(0,len(tmp),chunk_size)]
-			v_names = sub_lists[0][1:-2]
-			v_pvals = sub_lists[1][1:-2]
-			v_coefs = sub_lists[2][1:-2]
-			bbr_results[cgID] = [v_coefs,v_pvals]
-		else:
-			bbr_results[cgID] = [["NaN"]* len(cv_names), ["NaN"]* len(cv_names)]
-	
+			r_names = sub_lists[0]
+			r_pvalues = sub_lists[1]
+			r_coefs = sub_lists[2]	
+			for i in range(len(r_names)):
+				if r_names[i] not in cv_names:
+					continue
+				bbr_names.append(r_names[i])
+				bbr_pvalues.append(r_pvalues[i])
+				bbr_coeffs.append(r_coefs[i])
+		bbr_results[cgID] = [bbr_names, bbr_pvalues, bbr_coeffs]	
 	printlog("Results saved to \"%s\" ..." % (options.out_file + '.pval.txt'))
 	line_num = 0
 	for l in ireader.reader(options.input_file):
 		line_num += 1
 		f = l.split()
 		if line_num == 1:
-			print (l + '\t' + '\t'.join([i + '.coef' for i in v_names]) + '\t' + '\t'.join([i + '.pval' for i in v_names]), file=FOUT)
+			print (l + '\t' + '\t'.join([i + '.coef' for i in bbr_names]) + '\t' + '\t'.join([i + '.pval' for i in bbr_names]), file=FOUT)
 		else:
 			cgID = f[0]
-			print (l + '\t' + '\t'.join(bbr_results[cgID][0]) + '\t' + '\t'.join(bbr_results[cgID][1]), file=FOUT)
+			print (l + '\t' + '\t'.join(bbr_results[cgID][2]) + '\t' + '\t'.join(bbr_results[cgID][1]), file=FOUT)
 	
 	FOUT.close()
 
