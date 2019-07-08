de39751e499ef9f1cacbf5738bbca5813c430587
Wang <wangliguo78@gmail.com>
2019-07-07 22:56:31 -0500

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -171,7 +171,7 @@ html_last_updated_fmt = 'July 7, 2019'
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-html_show_sourcelink = True
+# html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = True
